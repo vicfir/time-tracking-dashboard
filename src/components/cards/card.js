e9f1a0141a76a2a3617e5de6e4e801b2f05dbc21
vicfir <vicfir@hotmail.fr>
2023-02-13 12:18:07 +0100

    adding img srrc in json + dispaly

--- src/components/cards/card.js
+++ src/components/cards/card.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const Card = (props) => {
   return (
     <div>
-        {/* <img src="./images/icon-work.svg" alt="" /> */}
+        <img src={props.image} alt="" />
         <h1>{props.title}</h1>
         <h2>{props.hours}hrs</h2>
         <p>Last Week - {props.last}hrs</p>
--- src/components/cards/cards.js
+++ src/components/cards/cards.js
@@ -5,7 +5,8 @@ export const Cards = () => {
     let data = require('./data/data.json');
     console.log(data);
 
-    let cardsData = data.map((cardData) =>(<Card title={cardData.title} hours={cardData.timeframes.weekly.current} last={cardData.timeframes.weekly.previous}/>));
+    let cardsData = data.map((cardData , index) =>(<Card key={index} image={cardData.image} title={cardData.title} hours={cardData.timeframes.weekly.current} last={cardData.timeframes.weekly.previous}/>));
+    console.log(cardsData)
     return (
         
         <div>
